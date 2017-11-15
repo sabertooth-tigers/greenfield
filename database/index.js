0ee0b15a01d4ed5f028f87c0e6183051210bce47
kevinfang <fangk1220@gmail.com>
2017-11-15 15:21:08 -0800

    Database now has a save function for the each schema

--- database/index.js
+++ database/index.js
@@ -7,6 +7,7 @@ dbConnect.once('open', () => {
   console.log('Successfully connected');
 });
 
+//  USERS defines what role a user should have.
 const USERS = ['generalUser', 'moderator', 'admin'];
 const userSchema = mongoose.Schema({
   // May not need unique
@@ -36,3 +37,46 @@ const commentSchema = mongoose.Schema({
   createdAt: Date,
   vote: Number,
 });
+
+//  constructor
+const UserModel = mongoose.model('UserModel', userSchema);
+const ThreadModel = mongoose.model('ThreadModel', threadSchema);
+const CommentModel = mongoose.model('CommentModel', commentSchema);
+
+// Do we need to return for save?
+const saveUser = (user) => {
+  const newUser = new UserModel({
+    role: user.role,
+    username: user.username,
+    password: user.password,
+    salt: user.salt,
+    email: user.email,
+    id: user.id,
+  });
+
+  newUser.save((err) => { if (err) return err; return true; });
+};
+
+const saveThread = (thread) => {
+  const newThread = new ThreadModel({
+    id: thread.id,
+    creatorId: thread.creator,
+    description: thread.description,
+    title: thread.title,
+    createdAt: thread.date,
+  });
+
+  newThread.save((err) => { if (err) return err; return true; });
+};
+
+const saveComment = (comment) => {
+  const newComment = new CommentModel({
+    id: comment.id,
+    text: comment.text,
+    userId: comment.userId,
+    createdAt: comment.date,
+    vote: Number,
+  });
+
+  newComment.save((err) => { if (err) return err; return true; });
+};
--- react-env/src/app.jsx
+++ react-env/src/app.jsx
@@ -1,3 +1,4 @@
+/* eslint-env browser */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Switch, Route, Link } from 'react-router-dom';
