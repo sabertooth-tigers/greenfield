66208a29eba7eb5a7fd483bbfc1e564b41b3fc99
kevinfang <fangk1220@gmail.com>
2017-11-15 13:35:22 -0800

    Working on schema

---
+++ database/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+//  You might have to clear your original test db or change this to a new database
+mongoose.connect('mongo://localhost/test');
+const dbConnect = mongoose.connection;
+dbConnect.on('error', console.error.bind('Connection error'));
+dbConnect.once('open', () => {
+  console.log('Successfully connected');
+});
+
+const userSchema = mongoose.Schema({
+
+});
