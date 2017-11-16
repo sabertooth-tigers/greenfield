98c1f5ddfa2d61bb4e71dac1593a856ea4f149f3
kevinfang <fangk1220@gmail.com>
2017-11-16 11:37:01 -0800

    Worked with Mike to create List and threadListEntry

--- react-env/src/components/List.jsx
+++ react-env/src/components/List.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-// import listEntry from './listEntry.jsx';
+import ListEntry from './threadListEntry.jsx';
 
-const List = (props) => (
+const List = ({ SOME_VAR }) => (
   //  CHANGE NAME OF VARIALBE WHEN DECIDED IN App
   //  Do we need () around {} after => ?
   //  USE index if _.id doesn't work. Assumptions that we will get _.id because of mongoose
-  props.SOME_VAR.map((threadEntry) => {
-    return (
-      <div>
-        <ThreadEntry entry={threadEntry} key={threadEntry._id} />
-      </div>
-    )
-  }
-)
-
+  //  CHANGE SOME_VAR to array passed in
+  props.SOME_VAR.map(threadEntry => (
+    <div>
+      <ThreadEntry entry={threadEntry} key={threadEntry._id} />
+    </div>
+  ))
+);
 export default List;
