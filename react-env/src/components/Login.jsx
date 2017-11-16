59ea0c227ce3491166dbe0530750b2d3d6126c8d
chalkera <achalker@gmail.com>
2017-11-16 10:33:18 -0800

    Added an email validator package and fleshed out login page more

--- react-env/src/components/Login.jsx
+++ react-env/src/components/Login.jsx
@@ -6,19 +6,45 @@ class Login extends React.Component {
     super();
 
     this.state = {
-      username: '',
+      email: '',
       password: '',
     };
+
+    this.emailQuery = this.emailQuery.bind(this);
+    this.passwordQuery = this.passwordQuery.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  emailQuery(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  passwordQuery(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  submit() {
+    $.ajax({
+      type: 'GET',
+      url: '/Users',
+      data: {
+        email: this.state.email,
+        password: this.state.password,
+      },
+    });
   }
 
   render() {
     return (
-      <div>
+      <div id="Login">
         <h2>Login</h2>
         <p>{this.state.error}</p>
-        Username: <input onChange={this.usernameQuery} type="text" />
+        email: <input onChange={this.emailQuery} type="text" />
         <br />
-        Password: <input onChange={this.passwordQuery} type="text" />
+        Password: <input onChange={this.passwordQuery} type="password" />
+        <br />
+        <button onClick={this.submit} >Login </button>
+        <div className="g-recaptcha" data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" />
       </div>
     );
   }
